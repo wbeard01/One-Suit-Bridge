7c5db90ffca24aca0b6c2068c32924a54eb395b3
William Beard <52893758+wbeard01@users.noreply.github.com>
2023-10-16 12:00:28 -0400

    Change leader based on who won trick

--- OptimalScore.java
+++ OptimalScore.java
@@ -1,98 +1,113 @@
-import java.util.HashMap;
+import java.util.*;
 
 public class OptimalScore {
 
     public static HashMap<String, Integer> memo;
+
     public static void main(String[] args) {
 
-        int n = 12;
-        int[] left = new int[n];
-        int[] right = new int[n];
-        for (int i = 1; i <= n; i++) {
-            left[i - 1] = 2 * i;
-            right[i - 1] = 2 * i - 1;
-        }
+        int[] left = { 2,4,6,8,10,12 };
+        int[] right = { 1,3,5,7,9,11 };
+        int n = left.length;
         memo = new HashMap<>();
-        System.out.println("Naive Result: " + (2 * naive(n, left, right, 0, 0, 0, 0) - n));
+        System.out.println("Naive Result: " + naive(n, left, right, 0, 0, 0, 0, 1));
         memo = new HashMap<>();
-        System.out.println("Pruned Result: " + (2 * pruned(n, left, right, 0, 0, 0, 0) - n));
+        System.out.println("Pruned Result: " + pruned(n, left, right, 0, 0, 0, 0, 1));
 
     }
 
-    public static int naive(int n, int[] left, int[] right, int turn, int leftUsed, int rightUsed, int leftInd) {
+    public static int naive(int n, int[] leaderCards, int[] followerCards, int turn, int leaderCardsUsed,
+            int followerCardsUsed, int leaderCardIndex, int currentLeaderMultiplier) {
         if (turn == 2 * n) {
             return 0;
         }
-        String key = leftUsed + " " + rightUsed + " " + leftInd;
+        String key = leaderCardsUsed + " " + followerCardsUsed + " " + leaderCardIndex + " " + currentLeaderMultiplier;
         if (!memo.containsKey(key)) {
-            int ans;
+            int bestScore;
             if (turn % 2 == 0) {
-                ans = -n;
+                bestScore = -n * currentLeaderMultiplier;
                 for (int i = 0; i < n; i++) {
-                    if ((leftUsed & (1 << i)) == 0) {
-                        int t = naive(n, left, right, turn + 1, leftUsed | (1 << i), rightUsed, i);
-                        ans = Math.max(ans, t);
+                    if ((leaderCardsUsed & (1 << i)) == 0) {
+                        int t = naive(n, leaderCards, followerCards, turn + 1, leaderCardsUsed | (1 << i),
+                                followerCardsUsed, i, currentLeaderMultiplier);
+                        if (t > bestScore && currentLeaderMultiplier == 1 || t < bestScore && currentLeaderMultiplier == -1) {
+                            bestScore = t;
+                        }
                     }
                 }
             } else {
-                ans = n;
+                bestScore = n * currentLeaderMultiplier;
                 for (int i = 0; i < n; i++) {
-                    if ((rightUsed & (1 << i)) == 0) {
-                        int t = naive(n, left, right, turn + 1, leftUsed, rightUsed | (1 << i), -1);
-                        if (left[leftInd] > right[i]) {
-                            t++;
+                    if ((followerCardsUsed & (1 << i)) == 0) {
+                        int t;
+                        if (leaderCards[leaderCardIndex] > followerCards[i]) {
+                            t = naive(n, leaderCards, followerCards, turn + 1, leaderCardsUsed,
+                                    followerCardsUsed | (1 << i), -1, currentLeaderMultiplier)
+                                    + currentLeaderMultiplier;
+                        } else {
+                            t = naive(n, followerCards, leaderCards, turn + 1, followerCardsUsed | (1 << i),
+                                    leaderCardsUsed, -1, currentLeaderMultiplier * -1) - currentLeaderMultiplier;
+
                         }
-                        ans = Math.min(ans, t);
+                        if (t < bestScore && currentLeaderMultiplier == 1 || t > bestScore && currentLeaderMultiplier == -1) {
+                            bestScore = t;
+                        }
                     }
                 }
             }
-            memo.put(key, ans);
+            memo.put(key, bestScore);
         }
         return memo.get(key);
     }
 
-    public static int pruned(int n, int[] left, int[] right, int turn, int leftUsed, int rightUsed, int leftInd) {
+    public static int pruned(int n, int[] leaderCards, int[] followerCards, int turn, int leaderCardsUsed,
+            int followerCardsUsed, int leaderCardIndex, int currentLeaderMultiplier) {
         if (turn == 2 * n) {
             return 0;
         }
-        String key = leftUsed + " " + rightUsed + " " + leftInd;
+        String key = leaderCardsUsed + " " + followerCardsUsed + " " + leaderCardIndex + " " + currentLeaderMultiplier;
         if (!memo.containsKey(key)) {
-            int ans;
+            int bestScore;
             if (turn % 2 == 0) {
-                ans = -n;
+                bestScore = -n * currentLeaderMultiplier;
                 for (int i = 0; i < n; i++) {
-                    if ((leftUsed & (1 << i)) == 0) {
-                        int t = pruned(n, left, right, turn + 1, leftUsed | (1 << i), rightUsed, i);
-                        ans = Math.max(ans, t);
+                    if ((leaderCardsUsed & (1 << i)) == 0) {
+                        int t = pruned(n, leaderCards, followerCards, turn + 1, leaderCardsUsed | (1 << i),
+                                followerCardsUsed, i, currentLeaderMultiplier);
+                        if (t > bestScore && currentLeaderMultiplier == 1 || t < bestScore && currentLeaderMultiplier == -1) {
+                            bestScore = t;
+                        }
                     }
                 }
             } else {
-                ans = n;
+                bestScore = n * currentLeaderMultiplier;
                 boolean lost = false;
                 boolean won = false;
                 for (int i = 0; i < n; i++) {
-                    if ((rightUsed & (1 << i)) == 0) {
-                        if (left[leftInd] < right[i] && !lost) {  
-                            int t = pruned(n, left, right, turn + 1, leftUsed, rightUsed | (1 << i), -1);
-                            if (left[leftInd] > right[i]) {
-                                t++;
+                    if ((followerCardsUsed & (1 << i)) == 0) {
+                        if (leaderCards[leaderCardIndex] < followerCards[i] && !lost) {
+                            int t = pruned(n, followerCards, leaderCards, turn + 1, followerCardsUsed | (1 << i),
+                                    leaderCardsUsed, -1, currentLeaderMultiplier * -1) - currentLeaderMultiplier;
+                            if (t < bestScore && currentLeaderMultiplier == 1 || t > bestScore && currentLeaderMultiplier == -1) {
+                                bestScore = t;
                             }
-                            ans = Math.min(ans, t);
+                            bestScore = Math.min(bestScore, t);
                             lost = true;
-                        } else if (left[leftInd] > right[i] && !won) {
-                            int t = pruned(n, left, right, turn + 1, leftUsed, rightUsed | (1 << i), -1);
-                            if (left[leftInd] > right[i]) {
-                                t++;
+                        } else if (leaderCards[leaderCardIndex] > followerCards[i] && !won) {
+                            int t = pruned(n, leaderCards, followerCards, turn + 1, leaderCardsUsed,
+                                    followerCardsUsed | (1 << i), -1, currentLeaderMultiplier)
+                                    + currentLeaderMultiplier;
+                            if (t < bestScore && currentLeaderMultiplier == 1 || t > bestScore && currentLeaderMultiplier == -1) {
+                                bestScore = t;
                             }
-                            ans = Math.min(ans, t);
                             won = true;
                         }
                     }
                 }
             }
-            memo.put(key, ans);
+            memo.put(key, bestScore);
         }
         return memo.get(key);
     }
-    
+
 }
